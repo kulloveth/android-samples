48fa0a87c7b7938e245d19f0b7160507166afdb6
kulloveth <kulloveth@gmail.com>
2021-05-04 21:13:16 +0100

    display alcoholic

--- CockTailApp/app/src/main/java/com/kulloveth/cocktailapp/fragments/MainFragment.kt
+++ CockTailApp/app/src/main/java/com/kulloveth/cocktailapp/fragments/MainFragment.kt
@@ -8,15 +8,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.kulloveth.cocktailapp.R
 import com.kulloveth.cocktailapp.data.api.Api
+import com.kulloveth.cocktailapp.data.api.CocktailInfoModel
 import com.kulloveth.cocktailapp.data.api.Repository
+import com.kulloveth.cocktailapp.databinding.FragmentMainBinding
+import com.kulloveth.cocktailapp.fragments.main.MainAdapter
 import com.kulloveth.cocktailapp.fragments.main.MainFragmentViewModel
 import com.kulloveth.cocktailapp.fragments.main.MainViewModelFactory
 
 
 class MainFragment : Fragment() {
 
+    private var binding:FragmentMainBinding? = null
+    val drinks = mutableListOf<CocktailInfoModel>()
 
     private val viewModel: MainFragmentViewModel by lazy {
         ViewModelProvider(
@@ -25,17 +31,25 @@ class MainFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding?.root
 
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.drinksLiveData.observe(this, Observer {
          Log.d("","${it.size}")
+            drinks.addAll(it)
+            binding?.pb?.visibility = View.GONE
+            binding?.cocktailRv?.visibility = View.VISIBLE
         })
+        val adapter:MainAdapter = MainAdapter(drinks)
+        binding?.cocktailRv?.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+        binding?.cocktailRv?.adapter = adapter;
+
     }
 }
---
+++ CockTailApp/app/src/main/java/com/kulloveth/cocktailapp/fragments/main/MainAdapter.kt
@@ -0,0 +1,30 @@
+package com.kulloveth.cocktailapp.fragments.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.kulloveth.cocktailapp.data.api.CocktailInfoModel
+import com.kulloveth.cocktailapp.databinding.ItemRvBinding
+
+class MainAdapter(private val drinks:List<CocktailInfoModel>):RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+    inner class MainViewHolder(private val binding: ItemRvBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(cocktailInfoModel: CocktailInfoModel){
+            binding.title.text = cocktailInfoModel.drinkName
+            binding.thumbIv.load(cocktailInfoModel.thumbNail)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        return MainViewHolder(ItemRvBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.bind(drinks[position])
+    }
+
+    override fun getItemCount(): Int {
+      return drinks.size
+    }
+}
